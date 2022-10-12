5bbc35646d8fd8dc1d28a3843ecdd8ae8d70d4b6
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-12 23:49:16 +0400

    Hover animation for ShowcaseAppItem

--- lib/widgets/showcase_app_item.dart
+++ lib/widgets/showcase_app_item.dart
@@ -4,7 +4,7 @@ import 'package:kamranbekirovcom_website/domain/showcase_app.dart';
 import 'package:kamranbekirovcom_website/screens/landing_screen.dart';
 import 'package:kamranbekirovcom_website/widgets/external_link_button.dart';
 
-class ShowcaseAppItem extends StatelessWidget {
+class ShowcaseAppItem extends StatefulWidget {
   final ShowcaseApp app;
 
   const ShowcaseAppItem(
@@ -13,73 +13,106 @@ class ShowcaseAppItem extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ShowcaseAppItem> createState() => _ShowcaseAppItemState();
+}
+
+class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
+  late bool _hovered;
+
+  @override
+  void initState() {
+    _hovered = false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(8.0),
-        topRight: Radius.circular(8.0),
-        bottomLeft: Radius.circular(4.0),
-        bottomRight: Radius.circular(4.0),
-      ),
-      child: Container(
-        color: cardColor,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            app.isNetworkImage
-                ? Image.network(
-                    app.image,
-                  )
-                : Image.asset(
-                    app.image,
-                  ),
-            Container(
-              height: 201.0,
-              padding: const EdgeInsets.all(24.0).copyWith(bottom: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    app.name,
-                    style: const TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white,
-                      letterSpacing: 1.4,
-                    ),
-                  ),
-                  const Divider(
-                    color: dividerColor,
-                    thickness: 1.5,
-                    height: 32.0,
-                  ),
-                  if (app.playStoreURL != null) ...[
-                    ExternalLinkButton(
-                      url: app.playStoreURL!,
-                      iconData: FontAwesomeIcons.googlePlay,
-                      label: 'Play Store',
-                    ),
-                    const SizedBox(height: 8.0),
-                  ],
-                  if (app.appStoreURL != null) ...[
-                    ExternalLinkButton(
-                      url: app.appStoreURL!,
-                      iconData: FontAwesomeIcons.appStoreIos,
-                      label: 'App Store',
-                    ),
-                    const SizedBox(height: 8.0),
-                  ],
-                  if (app.githubURL != null)
-                    ExternalLinkButton(
-                      url: app.githubURL!,
-                      iconData: FontAwesomeIcons.squareGithub,
-                      label: 'GitHub',
-                    ),
-                ],
-              ),
+    return MouseRegion(
+      onEnter: (event) => setState(() => _hovered = true),
+      onExit: (event) => setState(() => _hovered = false),
+      child: AnimatedScale(
+        duration: kThemeAnimationDuration,
+        scale: _hovered ? 1.02 : 1.0,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(8.0),
+            topRight: Radius.circular(8.0),
+            bottomLeft: Radius.circular(4.0),
+            bottomRight: Radius.circular(4.0),
+          ),
+          child: Container(
+            color: cardColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildImage(),
+                _buildBottom(),
+              ],
             ),
-          ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return widget.app.isNetworkImage
+        ? Image.network(
+            widget.app.image,
+          )
+        : Image.asset(
+            widget.app.image,
+          );
+  }
+
+  Widget _buildBottom() {
+    return Container(
+      height: 201.0,
+      padding: const EdgeInsets.all(24.0).copyWith(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildAppName(),
+          const Divider(
+            color: dividerColor,
+            thickness: 1.5,
+            height: 32.0,
+          ),
+          if (widget.app.playStoreURL != null) ...[
+            ExternalLinkButton(
+              url: widget.app.playStoreURL!,
+              iconData: FontAwesomeIcons.googlePlay,
+              label: 'Play Store',
+            ),
+            const SizedBox(height: 8.0),
+          ],
+          if (widget.app.appStoreURL != null) ...[
+            ExternalLinkButton(
+              url: widget.app.appStoreURL!,
+              iconData: FontAwesomeIcons.appStoreIos,
+              label: 'App Store',
+            ),
+            const SizedBox(height: 8.0),
+          ],
+          if (widget.app.githubURL != null)
+            ExternalLinkButton(
+              url: widget.app.githubURL!,
+              iconData: FontAwesomeIcons.squareGithub,
+              label: 'GitHub',
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppName() {
+    return Text(
+      widget.app.name,
+      style: const TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.w900,
+        color: Colors.white,
+        letterSpacing: 1.4,
       ),
     );
   }

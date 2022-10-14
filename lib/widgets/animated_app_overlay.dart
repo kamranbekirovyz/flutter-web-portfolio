923ad17782675831af711dab06d4b2ec0645c0c9
PORELARTE <kamranbekirovyz@gmail.com>
2022-10-14 23:36:56 +0400

    Improvements on AnimatedAppOverlay widget

--- lib/widgets/animated_app_overlay.dart
+++ lib/widgets/animated_app_overlay.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 
-class AnimatedAppOverlay extends StatelessWidget {
-  final bool hovered;
+class AnimatedAppOverlay extends StatefulWidget {
   final String topic;
 
-  const AnimatedAppOverlay({
+  const AnimatedAppOverlay(
+    this.topic, {
     Key? key,
-    required this.hovered,
-    required this.topic,
   }) : super(key: key);
 
   @override
+  State<AnimatedAppOverlay> createState() => _AnimatedAppOverlayState();
+}
+
+class _AnimatedAppOverlayState extends State<AnimatedAppOverlay> {
+  late bool _hovered;
+
+  @override
+  void initState() {
+    super.initState();
+    _hovered = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      duration: kThemeAnimationDuration,
-      opacity: hovered ? 1.0 : 0.0,
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.black54,
-        child: Text(
-          topic,
-          style: const TextStyle(
-            fontSize: 24.0,
-            color: Colors.white,
-            fontWeight: FontWeight.w800,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovered = true),
+      onExit: (_) => setState(() => _hovered = false),
+      child: AnimatedOpacity(
+        duration: kThemeAnimationDuration,
+        opacity: _hovered ? 1.0 : 0.0,
+        child: Container(
+          alignment: Alignment.center,
+          color: Colors.black54,
+          child: Text(
+            widget.topic,
+            style: const TextStyle(
+              fontSize: 24.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+            ),
           ),
         ),
       ),
--- lib/widgets/showcase_app_item.dart
+++ lib/widgets/showcase_app_item.dart
@@ -5,26 +5,13 @@ import 'package:kamranbekirovcom_website/screens/landing_screen.dart';
 import 'package:kamranbekirovcom_website/widgets/animated_app_overlay.dart';
 import 'package:kamranbekirovcom_website/widgets/external_link_button.dart';
 
-class ShowcaseAppItem extends StatefulWidget {
+class ShowcaseAppItem extends StatelessWidget {
   final ShowcaseApp app;
 
   const ShowcaseAppItem(
     this.app, {
     Key? key,
   }) : super(key: key);
-
-  @override
-  State<ShowcaseAppItem> createState() => _ShowcaseAppItemState();
-}
-
-class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
-  late bool _hovered;
-
-  @override
-  void initState() {
-    super.initState();
-    _hovered = false;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,24 +22,17 @@ class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
         bottomLeft: Radius.circular(4.0),
         bottomRight: Radius.circular(4.0),
       ),
-      child: MouseRegion(
-        onEnter: (_) => setState(() => _hovered = true),
-        onExit: (_) => setState(() => _hovered = false),
-        child: Stack(
-          children: [
-            _buildChild(),
-            Positioned(
-              top: 0.0,
-              bottom: 200.0,
-              left: 0.0,
-              right: 0.0,
-              child: AnimatedAppOverlay(
-                hovered: _hovered,
-                topic: widget.app.topic,
-              ),
-            ),
-          ],
-        ),
+      child: Stack(
+        children: [
+          _buildChild(),
+          Positioned(
+            top: 0.0,
+            bottom: 200.0,
+            left: 0.0,
+            right: 0.0,
+            child: AnimatedAppOverlay(app.topic),
+          ),
+        ],
       ),
     );
   }
@@ -71,12 +51,12 @@ class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
   }
 
   Widget _buildImage() {
-    return widget.app.isNetworkImage
+    return app.isNetworkImage
         ? Image.network(
-            widget.app.image,
+            app.image,
           )
         : Image.asset(
-            widget.app.image,
+            app.image,
           );
   }
 
@@ -93,25 +73,25 @@ class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
             thickness: 1.5,
             height: 32.0,
           ),
-          if (widget.app.playStoreURL != null) ...[
+          if (app.playStoreURL != null) ...[
             ExternalLinkButton(
-              url: widget.app.playStoreURL!,
+              url: app.playStoreURL!,
               iconData: FontAwesomeIcons.googlePlay,
               label: 'Play Store',
             ),
             const SizedBox(height: 10.0),
           ],
-          if (widget.app.appStoreURL != null) ...[
+          if (app.appStoreURL != null) ...[
             ExternalLinkButton(
-              url: widget.app.appStoreURL!,
+              url: app.appStoreURL!,
               iconData: FontAwesomeIcons.appStoreIos,
               label: 'App Store',
             ),
             const SizedBox(height: 10.0),
           ],
-          if (widget.app.githubURL != null)
+          if (app.githubURL != null)
             ExternalLinkButton(
-              url: widget.app.githubURL!,
+              url: app.githubURL!,
               iconData: FontAwesomeIcons.squareGithub,
               label: 'GitHub',
             ),
@@ -122,7 +102,7 @@ class _ShowcaseAppItemState extends State<ShowcaseAppItem> {
 
   Widget _buildAppName() {
     return Text(
-      widget.app.name,
+      app.name,
       style: const TextStyle(
         fontSize: 18.0,
         fontWeight: FontWeight.w900,
